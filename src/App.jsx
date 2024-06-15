b92b18fa86fc8999e75a68fa6eba334c7dcecadc
Lamia Fatiha <f6lamia@gmail.com>
2024-06-15 20:53:48 +0600

    todo list app html part done

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const App = () => {
+    return(
+        <>
+            <div className="main_div">
+                <div className="center_div">
+                    <br/>
+                    <h1>To Do List</h1>
+                    <br />
+                    <input type="text" placeholder='Add a item'/>
+                    <button>+</button>
+
+                    <ol>
+                        <li>buy apple</li>
+                    </ol>
+                </div>
+            </div>
+        </>
+    )
+
+};
+
+export default App;
