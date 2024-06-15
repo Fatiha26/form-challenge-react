33f600f8a2b960124a659af127b3ae36cf25d087
Lamia Fatiha <f6lamia@gmail.com>
2024-06-15 21:44:19 +0600

    add part  done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import Todolist from './Todolist';
 const App = () => {
+
+    const [inputList, setInputList] = useState("apple");
+    const [item, setItem] = useState([]);
+
+    const itemEvent = (event) => {
+        setInputList(event.target.value);
+    };
+
+    const listItem = () =>{
+        setItem((olditem) => {
+            return [...olditem, inputList];
+        }) ;
+        setInputList('');
+
+    };
+
     return(
         <>
             <div className="main_div">
@@ -8,11 +24,15 @@ const App = () => {
                     <br/>
                     <h1>To Do List</h1>
                     <br />
-                    <input type="text" placeholder='Add a item'/>
-                    <button>+</button>
+                    <input type="text" placeholder='Add a item' value={inputList} onChange={itemEvent}/>
+                    <button onClick={listItem}>+</button>
 
                     <ol>
-                        <li>buy apple</li>
+                    {item.map((itemval) => {
+                       return <Todolist 
+                            text ={itemval}
+                        />
+                    })}
                     </ol>
                 </div>
             </div>
